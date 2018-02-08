f2f63bf757fd57cc25d3fa3c235838cf7b882705
Vasil Zidrovski <vasildb@gmail.com>
2018-02-08 16:46:45 +0100

    test

--- callback.php
+++ callback.php
@@ -5,13 +5,23 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $last = get_last_block();
 
-if(verify_block($data) && $last['hash']===$data['previous_hash']) {
-	add_record($data);
-	echo 'Block added';
-} else {
-	echo 'Block refused';
+if(!verify_block($data)) {
+	echo 'Block not valid.';
+	exit;
 }
 
+if(find_by_hash($data['hash'])) {
+	echo 'Block already exists.';
+	exit;
+}
+
+if($last['hash']!==$data['previous_hash']) {
+	echo 'Block previous_hash not valid.';
+	exit;
+}
+
+add_record($data);
+echo 'Block added.';
 // @todo check hashes and insert data in db if correct
 // @todo broadcast the block to other nodes if correct, with guzzle send post to all the nodes in config, on /callback.php
 
--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include 'bootstrap.php';
 
-$block = ['name'=>'Broadcasted block', 'previous_hash'=>'9USrWjpG/c1zazelItNvOvVEq1o6Rv3Nc2s3pSLTbzo='];
+$block = ['name'=>'First product', 'previous_hash'=>'0'];
 $block['hash'] = make_hash($block);
 
 broadcast_block($block);
