f28362d757ac05e58818ce6282d43fe61da1869d
Hristijan <kde.chris@gmail.com>
2018-02-12 14:33:47 +0100

    add stellar transaction hash

--- update_block.php
+++ update_block.php
@@ -11,6 +11,7 @@ update_block($base, $post["stellar_transaction_hash"]);
 
 
 $latest = get_last_block();
+$latest["stellar_transaction_hash"] = $post["stellar_transaction_hash"];
 $broadcast_info = broadcast_block($latest);
 
 ?>
